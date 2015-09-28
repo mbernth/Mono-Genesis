ef2e1b6145a77174400cc4680d105ed88cd3b524
Michael Bernth <michael@monovoce.dk>
2015-09-28 19:20:31 +0200

    Thumbnail Grid added

--- mono-basic-theme/functions.php
+++ mono-basic-theme/functions.php
@@ -121,3 +121,114 @@ function cc_mime_types($mimes) {
   return $mimes;
 }
 add_filter('upload_mimes', 'cc_mime_types');
+
+//* Remove the edit link
+add_filter ( 'genesis_edit_post_link' , '__return_false' );	
+
+// check if the flexible content field has rows of data
+function mono_flexible_gridset() {
+	
+	if( have_rows('content') ):
+
+		// loop through the rows of data
+    	while ( have_rows('content') ) : the_row();
+
+        	if( get_row_layout() == 'full_width_column' ):
+				echo '<div class="gridcontainer">';
+					echo '<div class="gridcoll1">';
+        				the_sub_field('gridset_1_1');
+					echo '</div>';
+				echo '</div>';
+				
+        	elseif( get_row_layout() == 'two_columns' ):
+				echo '<div class="gridcontainer">';
+					echo '<div class="gridcoll2">';
+        				the_sub_field('gridset_2_1');
+					echo '</div>';
+					echo '<div class="gridcoll2">';
+						the_sub_field('gridset_2_2');
+					echo '</div>';
+				echo '</div>';
+				
+			elseif( get_row_layout() == 'three_columns' ):
+				echo '<div class="gridcontainer">';
+					echo '<div class="gridcoll3">';
+        				the_sub_field('gridset_3_1');
+					echo '</div>';
+					echo '<div class="gridcoll3">';
+						the_sub_field('gridset_3_2');
+					echo '</div>';
+					echo '<div class="gridcoll3">';
+						the_sub_field('gridset_3_3');
+					echo '</div>';
+				echo '</div>';
+				
+			elseif( get_row_layout() == 'four_columns' ):
+				echo '<div class="gridcontainer">';
+					echo '<div class="gridcoll4">';
+        				the_sub_field('gridset_4_1');
+					echo '</div>';
+					echo '<div class="gridcoll4">';
+						the_sub_field('gridset_4_2');
+					echo '</div>';
+					echo '<div class="gridcoll4">';
+						the_sub_field('gridset_4_3');
+					echo '</div>';
+					echo '<div class="gridcoll4">';
+						the_sub_field('gridset_4_4');
+					echo '</div>';
+				echo '</div>';
+				
+			elseif( get_row_layout() == 'five_columns' ):
+				echo '<div class="gridcontainer">';
+					echo '<div class="gridcoll5">';
+        				the_sub_field('gridset_5_1');
+					echo '</div>';
+					echo '<div class="gridcoll5">';
+						the_sub_field('gridset_5_2');
+					echo '</div>';
+					echo '<div class="gridcoll5">';
+						the_sub_field('gridset_5_3');
+					echo '</div>';
+					echo '<div class="gridcoll5">';
+						the_sub_field('gridset_5_4');
+					echo '</div>';
+					echo '<div class="gridcoll5">';
+						the_sub_field('gridset_5_5');
+					echo '</div>';
+				echo '</div>';
+				
+			elseif( get_row_layout() == 'six_columns' ):
+				echo '<div class="gridcontainer">';
+					echo '<div class="gridcoll6">';
+        				the_sub_field('gridset_6_1');
+					echo '</div>';
+					echo '<div class="gridcoll6">';
+						the_sub_field('gridset_6_2');
+					echo '</div>';
+					echo '<div class="gridcoll6">';
+						the_sub_field('gridset_6_3');
+					echo '</div>';
+					echo '<div class="gridcoll6">';
+						the_sub_field('gridset_6_4');
+					echo '</div>';
+					echo '<div class="gridcoll6">';
+						the_sub_field('gridset_6_5');
+					echo '</div>';
+					echo '<div class="gridcoll6">';
+						the_sub_field('gridset_6_6');
+					echo '</div>';
+				echo '</div>';
+				
+        	endif;
+
+    	endwhile;
+
+	else :
+
+    // no layouts found
+
+	endif;
+
+}
+add_action( 'genesis_entry_content', 'mono_flexible_gridset', 15 );
---
+++ mono-basic-theme/page_thumbnailgrid.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * This file adds the Landing template to the Danish Justice Foundation Pro Theme.
+ *
+ * @author mono voce aps
+ * @package Mono Basic Theme
+ * @subpackage Customizations
+*/
+
+/*
+Template Name: Thumbnail Grid
+*/
+
+//* Enqueue scripts and styles
+add_action( 'wp_enqueue_scripts', 'thumbnail_enqueue_scripts_styles' );
+function thumbnail_enqueue_scripts_styles() {
+	wp_enqueue_script( 'thumbnail-modernizr', get_bloginfo( 'stylesheet_directory' ) . '/js/modernizr.custom.js', array( 'jquery' ), '1.0.0' );
+	wp_enqueue_script( 'thumbnail-jquery', get_bloginfo( 'stylesheet_directory' ) . '/js/jquery.min.js', array( 'jquery' ), '1.0.0', true );
+	wp_enqueue_script( 'thumbnail-grid', get_bloginfo( 'stylesheet_directory' ) . '/js/grid.js', array( 'jquery' ), '1.0.0', true );
+	wp_enqueue_script( 'thumbnail-grid-init', get_bloginfo( 'stylesheet_directory' ) . '/js/grid_init.js', array( 'jquery' ), '1.0.0', true );
+
+}
+
+//* Add landing body class to the head
+add_filter( 'body_class', 'mono_add_body_class' );
+function mono_add_body_class( $classes ) {
+
+	$classes[] = 'thumbnail_grid';
+	return $classes;
+
+}
+
+//* Force full width content layout
+add_filter( 'genesis_site_layout', '__genesis_return_full_width_content' );
+
+// Thumbnail grid
+add_action( 'genesis_entry_content', 'mono_thumbnail_grid', 15 );
+function mono_thumbnail_grid() {
+	$thumbs = get_field( 'thumbnail' );
+	
+	if ( is_single() || is_page() ) {
+		
+		if($thumbs) {
+			echo '<ul id="og-grid" class="og-grid">';
+			
+			foreach($thumbs as $thumb) {
+				echo '<li>';
+					echo '<a href="';
+							echo '' . $thumb['link']. '';
+					echo '" data-largesrc="';	
+						echo '' . $thumb['image']. '';
+					echo '" data-title="';
+							echo '' . $thumb['titel']. '';
+					echo '" data-description="';								
+							echo '' . $thumb['description']. '';
+					echo '">';	
+					echo '<img src="';	
+						echo '' . $thumb['image']. '';
+					echo '"></a>';
+				echo '</li>';
+			}
+			
+			echo '</ul>';
+		}
+	}
+}
+
+//* Run the Genesis loop
+genesis();
